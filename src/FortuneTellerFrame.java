439fa9bb8eb04fa061bd06b9ad34d396ae053eed
mrale <mr.alexanderjavierl@gmail.com>
2024-02-12 22:23:21 -0500

    Initial commit

---
+++ src/FortuneTellerFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class FortuneTellerFrame {
+    public FortuneTellerFrame {
+        setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
